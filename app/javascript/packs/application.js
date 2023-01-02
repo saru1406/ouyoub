fe9330f2ac4b05033de729ed2c490e7386829fc2
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2023-01-02 04:48:17 +0000

    [add]投稿数を棒でグラフ追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+import jQuery from "jquery"
 import "popper.js"
 import "bootstrap"
-
+import Chart from 'chart.js/auto';
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+global.$ = jQuery;
+window.$ = jQuery;
+global.Chart = Chart;
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,23 +2,34 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   #今日の投稿数
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   #前日の投稿数
 	scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+	#2日前
+	scope :created_2days, -> { where(created_at: 2.day.ago.all_day) }
+	#3日前
+	scope :created_3days, -> { where(created_at: 3.day.ago.all_day) }
+	#4日前
+	scope :created_4days, -> { where(created_at: 4.day.ago.all_day) }
+	#5日前
+	scope :created_5days, -> { where(created_at: 5.day.ago.all_day) }
+	#6日前
+	scope :created_6days, -> { where(created_at: 6.day.ago.all_day) }
 	#今週の投稿数
-	scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+	scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
 	#前週の投稿数
-  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
-	
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
